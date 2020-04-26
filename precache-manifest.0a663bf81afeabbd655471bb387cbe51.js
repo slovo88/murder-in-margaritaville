53b525b65dd5cfb811f22e355357069a6f3ac5c4
slovo88 <prmoore45@gmail.com>
2020-04-26 17:17:40 -0400

    Updates

---
+++ precache-manifest.0a663bf81afeabbd655471bb387cbe51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68306acb2374cf7ec7bd99322e520ad",
+    "url": "/murder-in-margaritaville/index.html"
+  },
+  {
+    "revision": "9bf5dcef3014991baf76",
+    "url": "/murder-in-margaritaville/static/css/main.46d68f6b.chunk.css"
+  },
+  {
+    "revision": "63f306c25b1bba00c054",
+    "url": "/murder-in-margaritaville/static/js/2.90552edb.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/murder-in-margaritaville/static/js/2.90552edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf5dcef3014991baf76",
+    "url": "/murder-in-margaritaville/static/js/main.b406d103.chunk.js"
+  },
+  {
+    "revision": "3568c53afe34f40cbaf5",
+    "url": "/murder-in-margaritaville/static/js/runtime-main.0226b7bc.js"
+  }
+]);
